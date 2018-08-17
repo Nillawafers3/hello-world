99db87f094c19c9dafaa1c8a6470d1a998dfe9c6
Hyunwook Shin <Hyunwook-jiballack@gmail.com>
2018-08-17 08:47:11 -0700

    I added TestGit C++ file

---
+++ TestGit.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Test Git
+//
+//  Created by Bill Shin on 8/17/18.
+//  Copyright © 2018 Bill Shin. All rights reserved.
+//
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(int argc, const char * argv[]) {
+    cout << "This is a test for Github" << endl;
+    return 0;
+}
